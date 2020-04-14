b0f32823aa851c2d8ee669bb42c8b42ce914ec44
unknown <damianbe@hotmail.com>
2020-04-14 16:45:45 -0400

    se agrego lo demas

---
+++ form-to-email.php
@@ -0,0 +1,47 @@
+<?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+
+$email_from = 'sistemas@yarascorp.com';//<== update the email address
+$email_subject = "Entregado el cafe";
+$email_body = "la persona o empresa se llama $name.\n".
+    "y su email es:\n $visitor_email".
+    
+$to = "kerbero79@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
